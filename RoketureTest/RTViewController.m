06a9313818d10a236dc3ab289baa772a4cf033dd
Andrii Titov <concuror@gmail.com>
2013-08-12 22:45:09 +0300

    added auth request

--- RoketureTest/RTViewController.m
+++ RoketureTest/RTViewController.m
@@ -10,7 +10,8 @@
 #import "RTUser.h"
 #import "NSString+MD5.h"
 
-#define API_URL @"https://api.rocketroute.com/wx/v1"
+#define API_URL @"https://apidev.rocketroute.com/wx/v1"
+#define AUTH_URL @"https://mobiledev.rocketroute.com"
 
 @interface RTViewController () {
     NSMutableString *response;
@@ -29,10 +30,74 @@
 
 - (IBAction)createUser:(id)sender {
     RTUser *user = [RTUser user];
-    user.mail = work.text;
-    user.passw = [password.text toMD5];
-    password.hidden = YES;
+    if ([user.mail length] <= 0) {
+        if ([work.text length] + [password.text length] <= 0) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Please, enter correct ICAO" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+            [alert show];
+            return;
+        }
+        user.mail = work.text;
+        user.passw = [password.text toMD5];
+        password.hidden = YES;
+        work.enabled = NO;
+        loginButton.titleLabel.text = @"Logout";
+        [self makeLoginRequest];
+    } else {
+        [self clearUser];
+    }
     work.text = nil;
+    password.text = nil;
+}
+
+- (void)clearUser {
+    RTUser *user = [RTUser user];
+    user.mail = nil;
+    user.passw = nil;
+    work.enabled = YES;
+    password.hidden = NO;
+    loginButton.titleLabel.text = @"Login";
+}
+
+- (void)makeLoginRequest {
+    AFHTTPClient *tmpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:AUTH_URL]];
+    static NSString *authTemplate =@"<AUTH>\
+     <USR>%@</USR>\
+    <PASSWD>%@</PASSWD>\
+    <DEVICEID>%@</DEVICEID>\
+    <PCATEGORY>RocketRoute</PCATEGORY>\
+    <APPMD5>9193a8ec39d7dd2a5aefb28dfdffed7e</APPMD5>\
+    </AUTH>";
+    NSString *devID;
+    UIDevice *device = [UIDevice currentDevice];
+    if ([device respondsToSelector:@selector(identifierForVendor)]) {
+        devID = [[device identifierForVendor] UUIDString];
+    } else {
+        devID = [device uniqueIdentifier];
+    }
+    RTUser *user = [RTUser user];
+    NSString *reqvBody = [NSString stringWithFormat:authTemplate,user.mail,user.passw,devID];
+    NSMutableURLRequest *reqv = [tmpClient requestWithMethod:@"POST" path:@"remote/auth" parameters:nil];
+    [reqv setHTTPBody:[reqvBody dataUsingEncoding:NSUTF8StringEncoding]];
+    AFXMLRequestOperation *opertaion = [[AFXMLRequestOperation alloc] initWithRequest:reqv];
+    [opertaion setCompletionBlockWithSuccess:
+     ^(AFHTTPRequestOperation *operation, id XML) {
+         if ([XML isKindOfClass:[NSXMLParser class]]) {
+             NSXMLParser *parser = XML;
+             parser.delegate = self;
+             [parser parse];
+         } else {
+             [self clearUser];
+             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Response was not in XML format" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+             [alert show];
+         }
+     }
+                                     failure:
+     ^(AFHTTPRequestOperation *operation, NSError *error) {
+         NSLog(@"%@",error);
+         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+         [alert show];
+     }];
+    [opertaion start];
 }
 
 - (IBAction)getWeather:(id)sender {
@@ -41,7 +106,6 @@
         [alert show];
         return;
     }
-    //<request xmlns=\"urn:xmethods-wx">UKLL</request>
     static NSString *envelope = @"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tns=\"urn:wx/v1/\" xmlns:soap=\"http://schemas.xmlsoap.org/wsdl/soap/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:wsdl=\"http://schemas.xmlsoap.org/wsdl/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><SOAP-ENV:Body><request xmlns=\"urn:xmethods-wx\">%@</request></SOAP-ENV:Body></SOAP-ENV:Envelope>";
     static NSString *format = @"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
     <REQWX><USR>%@</USR><PASSWD>%@</PASSWD><ICAO>%@</ICAO></REQWX>";
@@ -50,7 +114,6 @@
     reqvBody = [NSString stringWithFormat:envelope,[reqvBody xmlSimpleEscape]];
     NSMutableURLRequest *reqv = [client requestWithMethod:@"POST" path:@"" parameters:nil];
     [reqv setHTTPBody:[reqvBody dataUsingEncoding:NSUTF8StringEncoding]];
-    //[reqv addValue:@"application/soap+xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
     [reqv addValue:@"urn:xmethods-wx#getWx" forHTTPHeaderField:@"SOAPAction"];
     AFXMLRequestOperation *opertaion = [[AFXMLRequestOperation alloc] initWithRequest:reqv];
     [opertaion setCompletionBlockWithSuccess:
@@ -117,6 +180,14 @@
         if (response.length > 0) {
             [weather setObject:response forKey:@"TAF"];
         }
+    } else if ([elementName isEqualToString:@"RESULT"]) {
+        if ([[[response stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] lowercaseString] isEqualToString:@"error"]) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, something went wrong. Please try again" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            [alert show];
+            [self clearUser];
+        }
+    } else if (@"AUTH") {
+        work.enabled = YES;
     }
 }
 
